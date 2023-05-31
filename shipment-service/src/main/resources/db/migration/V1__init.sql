2a163e0ed32c3033ebb783179978825e4cf481e3
Toshiaki Maki <tmaki@vmware.com>
2023-05-31 10:26:59 +0900

    Add unique constraint

--- shipment-service/src/main/resources/db/migration/V1__init.sql
+++ shipment-service/src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE shipment
 (
     shipment_id BIGSERIAL                NOT NULL PRIMARY KEY,
-    order_id    BIGSERIAL                NOT NULL,
+    order_id    BIGSERIAL                NOT NULL UNIQUE,
     order_date  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
